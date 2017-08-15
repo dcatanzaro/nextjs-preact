7ff4f5100634645ec047d4babc936fc754941a88
Damian <damiancatanzaro@gmail.com>
2017-08-15 20:08:02 -0300

    Hello World

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import Link from 'next/link';
 
 export default () => (
-  <div>asd World. <Link href='/about'><a>About</a></Link></div>
+  <div>Hello World. <Link href='/about'><a>About</a></Link></div>
 )
